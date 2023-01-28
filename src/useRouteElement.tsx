c3cf77d293bd62b06d034010b1ac4b4fe02db27e
Khanh Duy <tranhakhanhduy2001@gmail.com>
2023-01-28 17:11:58 +0700

    initialize boilerplate

---
+++ src/useRouteElement.tsx
@@ -0,0 +1,27 @@
+import { useRoutes } from 'react-router-dom'
+import RegisterLayout from './layouts/RegisterLayout'
+import Login from './pages/Login'
+import ProductList from './pages/ProductList'
+import Register from './pages/Register'
+export default function useRouteElement() {
+  const routeElement = useRoutes([
+    { path: '/', element: <ProductList /> },
+    {
+      path: '/login',
+      element: (
+        <RegisterLayout>
+          <Login />
+        </RegisterLayout>
+      )
+    },
+    {
+      path: '/register',
+      element: (
+        <RegisterLayout>
+          <Register />
+        </RegisterLayout>
+      )
+    }
+  ])
+  return routeElement
+}
